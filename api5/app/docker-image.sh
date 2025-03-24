8a9ffa98639a8f2a0b0e05155740cf2120ceaa6d
narasimhavivekt <cloudopscoin@gmail.com>
2025-03-24 19:32:39 +0000

    added docker build script to ecr

---
+++ api5/app/docker-image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+export AWS_ACCOUNT=""
+export AWS_DEFAULT_REGION="eu-west-2"
+export SERVICE_NAME="api5"
+export SERVICE_VERSION="${SERVICE_VERSION:-latest}"
+export IMAGE_NAME="$AWS_ACCOUNT.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/stockapi:${SERVICE_NAME}_${SERVICE_VERSION}"
+
+aws ecr get-login-password | docker login --username AWS --password-stdin "$AWS_ACCOUNT.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
+docker build -t $IMAGE_NAME .
+docker push $IMAGE_NAME
